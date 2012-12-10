8961b0532e1fa8ffeaef6fd8f22ec783568f169e
Emilio López <elopez93@gmail.com>
2012-12-09 23:08:18 -0300

    Add comments to mem/regs header files

--- libminivm/mem.h
+++ libminivm/mem.h
@@ -1,8 +1,20 @@
 #include <stdint.h>
 
-#define MEM_SIZE (640 * 1024)
+#ifndef __MEM_H__
+#define __MEM_H__
 
+/* Machine memory in bytes (640k default) */
+#ifndef MEM_SIZE
+#define MEM_SIZE    (640 * 1024)
+#endif
+
+/* Machine memory */
 char mem[MEM_SIZE];
 
+/* User-friendly macro to access bytes */
 #define MEM(i)      mem[(i)]
+
+/* User-friendly macro to access 32 bits */
 #define MEM32(i)    *(uint32_t*)(mem + (i))
+
+#endif
--- libminivm/regs.h
+++ libminivm/regs.h
@@ -1,5 +1,12 @@
 #include <stdint.h>
 
+#ifndef __REGS_H__
+#define __REGS_H__
+
+/* Machine registers. FLAGS is the last one on the enum */
 uint32_t regs[FLAGS + 1];
 
+/* User-friendly macro to access registers */
 #define REG(i)  regs[(i)]
+
+#endif
