6f3b3d49a676417e1f5cb5bc8a306095568cd808
Emilio López <elopez93@gmail.com>
2012-12-26 14:54:57 -0300

    Make the memory check build-selectable

--- minivm/mem.h
+++ minivm/mem.h
@@ -19,12 +19,16 @@ char mem[MEM_SIZE];
 /* User-friendly macro to access 32 bits */
 #define MEM32(i)    *(int32_t*)(mem + (i))
 
-#define MEMORY_CHECK(address) do { \
+#ifdef WITH_MEMORY_CHECK
+# define MEMORY_CHECK(address) do { \
     if ((address) >= MEM_SIZE) { \
         fprintf(stderr, "An invalid memory address was detected. The machine will now abort.\n"); \
         abort(); \
     } \
 } while(0)
+#else
+# define MEMORY_CHECK(address)
+#endif
 
 
 #endif
